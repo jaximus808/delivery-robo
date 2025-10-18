367204b6eea845c59298ea6584b8b944991d7520
jik07 <jonaskim777@gmail.com>
2025-10-18 17:51:41 -0500

    changed command args from int to double, initial motor control stuff

--- firmware/motor-controller/include/motor_driver.h
+++ firmware/motor-controller/include/motor_driver.h
@@ -1,4 +1,20 @@
 // Motor pins
 #define DRIVE_MOTOR 0
+#define TURN_MOTOR 0
+#define KP_V 2
+#define KI_V 5
+#define KD_V 1
+#define KP_T 2
+#define KI_T 5
+#define KD_T 1
+
 
 void setMotorPWM(int motor, int pwm);
+
+void setDriveVel(double speed);
+
+void setTurnAngle(double angle);
+
+void motorInit();
+
+void motorUpdate();
