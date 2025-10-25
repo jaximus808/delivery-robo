1380029103c7341449e9b811d6977ae78f58ee8a
alston <alstonliu5555@gmail.com>
2025-10-25 22:56:25 +0000

    protocol changes (and some fixes on Cmake)

--- firmware/motor-controller/commands.h
+++ firmware/motor-controller/commands.h
@@ -1,10 +1,20 @@
-#define ARG_MAX_LEN 16 // maximum # of characters in an argument
-#define NEW_COMMAND '\n' // newline
-#define NEW_ARG ' '
-#define ERROR_RES 'X'
-#define SET_DRIVE_PWM 'p'
-#define SET_DRIVE_VEL 'v'
-#define SET_TURN_ANGLE 'r'
+#ifndef COMMANDS_H
+#define COMMANDS_H
+
+#define ARG_MAX_LEN 16       // max chars in an argument
+#define NEW_COMMAND '\r'     // matches ArduinoComms termination
+#define NEW_ARG ' '          // argument separator
+#define ERROR_RES 'X'        // error response
+
+// Motor control
+#define SET_DRIVE_PWM 'm'    // "m val1 val2"
+#define SET_DRIVE_VEL 'v'    // "v val"
+#define SET_TURN_ANGLE 'r'   // "r val"
+#define SET_PID 'u'          // "u kp:kd:ki:ko"
+
+// Sensors / testing
 #define GET_ENCODER 'e'
 #define TEST_BLINK_ON 'o'
 #define TEST_BLINK_OFF 'f'
+
+#endif
--- firmware/motor-controller/motor-controller.ino
+++ firmware/motor-controller/motor-controller.ino
@@ -1,80 +1,71 @@
 #include "commands.h"
 #include "motor_driver.h"
 
-
 #define BAUDRATE 57600
 
-/* Command parsing variables */
-
-// A pair of varibles to help parse serial commands (thanks Fergs)
+// Command parsing variables
 int arg = 0;
 int index = 0;
-
-// Variable to hold an input character
 char chr;
-
-// Variable to hold the current single-character command
 char cmd;
-
-// Character arrays to hold the first and second arguments
 char argv1[ARG_MAX_LEN];
 char argv2[ARG_MAX_LEN];
-
-// The arguments converted to doubles
 double arg1;
 double arg2;
 
-
-// Run command based on currently parsed command:
 void runCommand();
-
-// Reset command variables to parse a new command
 void resetCommand();
-
-// Error parsing command, send back an error over serial
 void errorCommand();
-
-// Go through buffer, parse and run commands as needed
 void processBuffer();
 
 void setup() {
   Serial.begin(BAUDRATE);
   pinMode(LED_BUILTIN, OUTPUT);
-  // motorInit();
+  motorInit(); // initialize motors
 }
 
 void loop() {
   processBuffer();
-  // motorUpdate();
+  motorUpdate(); // keep PID running
 }
 
 void runCommand() {
   switch (cmd) {
-  case SET_DRIVE_PWM:
-    // int s = (int) arg1;
-    // if (s < 0 || s > 255) errorCommand();
-    // setMotorPWM(DRIVE_MOTOR, arg1);
-    break;
-  case SET_DRIVE_VEL:
-    // setDriveVel(arg1);
-    break;
-  case SET_TURN_ANGLE:
-    // setTurnAngle(arg2);
-    break;
-  case GET_ENCODER:
-    //TODO
-  case TEST_BLINK_ON:
-    digitalWrite(LED_BUILTIN, HIGH);
-    Serial.println("test on");
-    Serial.println(String(arg1, 5));
-    Serial.println(String(arg2, 5));
-    break;
-  case TEST_BLINK_OFF:
-    digitalWrite(LED_BUILTIN, LOW);
-    Serial.println("test off");
-    break;
-  default:
-    errorCommand();
+    case SET_DRIVE_PWM: // "m val1 val2"
+      setMotorPWM(DRIVE_MOTOR, (int)arg1);
+      setMotorPWM(TURN_MOTOR, (int)arg2);
+      break;
+
+    case SET_DRIVE_VEL: // "v val"
+      setDriveVel(arg1);
+      break;
+
+    case SET_TURN_ANGLE: // "r val"
+      setTurnAngle(arg1);
+      break;
+
+    case SET_PID: // "u kp:kd:ki:ko"
+      // TODO: parse colon-delimited values and call setPidValues()
+      break;
+
+    case GET_ENCODER: // "e"
+      Serial.print(getDriveVel());
+      Serial.print(" ");
+      Serial.println(getTurnAngle());
+      break;
+
+    case TEST_BLINK_ON: // "o"
+      digitalWrite(LED_BUILTIN, HIGH);
+      Serial.println("test on");
+      break;
+
+    case TEST_BLINK_OFF: // "f"
+      digitalWrite(LED_BUILTIN, LOW);
+      Serial.println("test off");
+      break;
+
+    default:
+      errorCommand();
   }
 }
 
@@ -97,19 +88,20 @@ void errorCommand() {
 void processBuffer() {
   while (Serial.available() > 0) {
     chr = Serial.read();
-    // New command/run command
+
+    // Ignore newline (\n) if present
+    if (chr == '\n') continue;
+
     if (chr == NEW_COMMAND) {
-      // Terminate arg array with null byte
       if (arg == 1) argv1[index] = '\0';
       else if (arg == 2) argv2[index] = '\0';
-      // Convert arg array to double
+
       arg1 = atof(argv1);
       arg2 = atof(argv2);
-      // Run command if a command is set
+
       if (cmd != '\0') runCommand();
       resetCommand();
     }
-    // New arg
     else if (chr == NEW_ARG) {
       if (arg == 0) arg++;
       else if (arg == 1) {
@@ -118,18 +110,14 @@ void processBuffer() {
         index = 0;
       }
     }
-    // Processing other characters
     else {
       if (arg == 0) {
-        // Error if command is already set
         if (cmd != '\0') errorCommand();
         else cmd = chr;
       } else if (arg == 1) {
-        argv1[index] = chr;
-        index++;
+        argv1[index++] = chr;
       } else if (arg == 2) {
-        argv2[index] = chr;
-        index++;
+        argv2[index++] = chr;
       }
 
       if (index == ARG_MAX_LEN) errorCommand();
